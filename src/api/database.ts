4d361ee547f7e2a7bd9e3f5f83121ffbdaab5909
Martial MBE <def.martial@gmail.com>
2024-09-17 17:23:11 +0200

    correction database

--- src/api/database.ts
+++ src/api/database.ts
@@ -1,15 +1,18 @@
 import { EsperoDB } from "esperodb";
 
-const dataStructure = [
-    {
-        'videos': [
-            {
-                indexes: [ { category: { unique: false } } ],
-                primaryKey: "_id"
-            }
-        ]
-    }
-];
 
 const dbVersion: number = 3;
-export const db = new EsperoDB('ouitube', dataStructure, dbVersion);
+export const db = new EsperoDB('ouitube', [
+    {
+      'videos': [
+        {
+          indexes: [
+
+            { slug: { unique: true } },
+            { category: { unique: false } }
+          ],
+          primaryKey: "_id"
+        }
+      ]
+    }
+  ], dbVersion);
